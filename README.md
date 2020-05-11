b0e59e8a825713ba0d346297f06d82655d257fc8
592193834@qq.com <592193834@qq.com>
2020-05-11 22:51:02 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,11 @@
+## 常用数据类型判断
+| 函数名 ｜说明｜
+| :----: ｜ :----: ｜
+| isArray: ｜   判断是否是 array 类型 ｜
+| isObject |   判断是否为 object 对象 |
+| isString |   判断是否为 string |
+| isPromise |  判断是否为 promise |
+| isSymbol |   判断是否是symbol |
+ |isFunc|      判断是否是函数 |
+| canParseJson | 能否json化 |
+| isTelNum |   判断是否符合手机号格式标准 |
---
+++ index.js
@@ -0,0 +1,35 @@
+import {
+  isArray,
+  isObject,
+  isString,
+  isEmptyObject,
+  isSymbol,
+  isFunc,
+  isPromise,
+  canParseJson,
+  isTelNum,
+  isIOS,
+  isAndroid,
+} from './is';
+
+import { getIn, setIn, deleteIn, compose } from './manipulate';
+
+import { buildQueryString } from './url';
+export {
+  isArray,
+  isObject,
+  isString,
+  isEmptyObject,
+  isSymbol,
+  isFunc,
+  isPromise,
+  canParseJson,
+  isTelNum,
+  isIOS,
+  isAndroid,
+  getIn,
+  setIn,
+  deleteIn,
+  compose,
+  buildQueryString,
+};
